d5bee7904670c51523623f32e9828bc15d21f57b
rikiachmad <rikiachmad@yahoo.com>
2021-12-13 22:47:13 +0700

    API Admin Done

--- routes/queues.js
+++ routes/queues.js
@@ -11,7 +11,7 @@ const { check, validationResult } = require('express-validator');
 // @access  Public
 router.get('/', async (req, res) => {
   try {
-    const queues = await Queue.find().select({ user: 0 });
+    const queues = await Queue.find().select();
     res.json(queues);
   } catch (err) {
     console.error(err.message);
@@ -37,45 +37,62 @@ router.get('/:id', auth, async (req, res) => {
 // @access  Private
 router.post(
   '/',
-  auth,
+  [
+    auth,
+    check('name', 'Please add name').not().isEmpty(),
+    check('address', 'Please add address').not().isEmpty(),
+    check('phone', 'Please add phone').not().isEmpty(),
+  ],
   async (req, res) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
 
     let checkUserId = await User.findById(req.user.id).select('-password');
-    if(!checkUserId){
+    let checkAdminId = await Admin.findById(req.user.id).select('-password');
+    if(!checkUserId && !checkAdminId){
       return res.status(400).json({ msg: 'User not found' });
     }
     const { status } = req.body;
 
-    let userOne = await User.findById(req.user.id);
-    let name = userOne.name;
-    let address = userOne.address;
-    let phone = userOne.phone;
-
+    var userOne;
+    var newname;
+    var newaddress;
+    var newphone;
+    if(checkAdminId){
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const { name, address, phone } = req.body;
+      newname = name;
+      newaddress = address;
+      newphone = phone;
+    }
+    else{
+      userOne = await User.findById(req.user.id);
+      newname = userOne.name;
+      newaddress = userOne.address;
+      newphone = userOne.phone;
+    }
     var num;
     var oldestQueue = await Queue.find().sort({ _id: -1 }).limit(1);
     if (oldestQueue.length) {
       num = oldestQueue[0].num + 1;
     } else num = 1;
+
     try {
       let checkUser = await Queue.findOne({ user: req.user.id });
-      if(checkUser){
+      if(checkUser && checkUserId){
         return res.status(400).json({ msg: 'User already have queue' });
       }
       const newQueue = new Queue({
-        name,
-        address,
-        phone,
+        name: newname,
+        address: newaddress,
+        phone: newphone,
         status,
         num,
         user: req.user.id,
       });
 
-      const queue = await newQueue.save();
-
+      const queue = await newQueue.save(); 
       res.json(queue);
     } catch (err) {
       console.error(err.message);
